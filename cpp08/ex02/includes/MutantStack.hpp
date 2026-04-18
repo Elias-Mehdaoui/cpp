af66e529b0e850735ac2dbab2c5d1f0803193ec8
Your Name <you@example.com>
2026-04-18 22:26:30 +0200

    pret pour push

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -1,3 +1,6 @@
+#ifndef MUTANTSTACK_HPP_
+#define MUTANTSTACK_HPP_
+
 #include <stack>
 #include <iostream>
 
@@ -7,13 +10,19 @@ class MutantStack: public std::stack<T>
 {
     public:
         MutantStack(){};
-        MutantStack(const MutantStack& other){(void)other;};
-        MutantStack &operator=(const MutantStack& other){(void)other;};
+        MutantStack(const MutantStack& other) : std::stack<T>(other) {};
+        MutantStack& operator=(const MutantStack& other)
+        {
+            std::stack<T>::operator=(other);
+            return *this;
+        };
 		~MutantStack(){};
 		typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
         iterator begin(void){return this -> c.begin();}
         iterator end(void){return this -> c.end();}
-        const iterator cbegin(void){return this -> c.begin();}
-        const iterator cend(void){return this -> c.end();}
+        const_iterator cbegin(void){return this -> c.begin();}
+        const_iterator cend(void){return this -> c.end();}
 };
+
+#endif
